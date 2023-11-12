ee774138d02d7ffa0e10b5d08e2fa48884887114
Guillaume <1017720+gwilherm@users.noreply.github.com>
2023-11-12 23:49:28 +0100

    Add button to switch piano mode

--- touchpad-piano/touchpad-piano.ino
+++ touchpad-piano/touchpad-piano.ino
@@ -1,4 +1,5 @@
 #include <Control_Surface.h>
+
 #include "MIDITouchpadPiano.hpp"
 
 // Instantiate a MIDI over USB interface.
@@ -7,13 +8,19 @@ USBMIDI_Interface midi;
 #define SCL_PIN 3
 #define SDO_PIN 2
 
-MIDITouchpadPiano piano(SCL_PIN, SDO_PIN, MIDI_Notes::C(4), PianoMode::Hold);
+#define BTN_PIN 5
+
+MIDITouchpadPiano piano(SCL_PIN, SDO_PIN, MIDI_Notes::C(4));
+
+int btnState = LOW;
+unsigned long lastDebounceTime = 0;
 
 void setup()
 {
   /* Initialise the serial interface */
   // Serial.begin(9600);
-  
+  pinMode(BTN_PIN, INPUT_PULLUP);
+
   Control_Surface.begin(); // Initialize Control Surface
 
   // while (!Serial);
@@ -21,5 +28,25 @@ void setup()
 
 void loop()
 {
+  int newBtnState = digitalRead(BTN_PIN);
+
+  if ((millis() - lastDebounceTime) > 50)
+  {
+    if ((btnState == LOW) && (newBtnState == HIGH))
+    {
+      switch (piano.getMode())
+      {
+        case PianoMode::Standard:
+          piano.setMode(PianoMode::Hold);
+          break;
+        case PianoMode::Hold:
+          piano.setMode(PianoMode::Standard);
+          break;
+      }
+    }
+    btnState = newBtnState;
+    lastDebounceTime = millis();
+  }
+
   Control_Surface.loop(); // Update the Control Surface
 }
