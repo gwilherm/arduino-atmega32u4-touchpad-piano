ae242280787a126c1691aafce0511ea884d03999
Guillaume <1017720+gwilherm@users.noreply.github.com>
2023-11-12 16:14:50 +0100

    Holding the chord

--- touchpad-piano/touchpad-piano.ino
+++ touchpad-piano/touchpad-piano.ino
@@ -9,8 +9,14 @@ USBMIDI_CREATE_DEFAULT_INSTANCE();
 #define MIDI_CHANNEL 1
 #define NB_NOTES     12
 
+#define HOLD_THRESHOLD_MS 500
+#define C4                 60
+#define VELOCITY           64
+
 byte NOTE_POS[NB_NOTES] = {1,3,6,8,10,0,2,4,5,7,9,11};
 byte keys_state[NB_NOTES] = {0};
+
+uint64_t lastTouchMs = 0;
 
 void setup()
 {
@@ -21,6 +27,8 @@ void setup()
   pinMode(SDO_PIN, INPUT);
   
   MIDI.begin(MIDI_CHANNEL_OMNI);
+
+  // while (!Serial);
 }
 
 void loop()
@@ -28,29 +36,48 @@ void loop()
   byte new_keys_state[NB_NOTES];
   byte i;
 
+  uint64_t prevTouch = lastTouchMs;
   // Read data
   for (i = 0; i < NB_NOTES; i++)
   {
     digitalWrite(SCL_PIN, LOW);
     new_keys_state[NOTE_POS[i]] = !digitalRead(SDO_PIN);
+    if (new_keys_state[NOTE_POS[i]]) lastTouchMs = millis();
     digitalWrite(SCL_PIN, HIGH);
   }
 
+  
   for (i = 0; i < NB_NOTES; i++)
   {
-    // Display input data
-    Serial.print(new_keys_state[i]);
+
+    // Re-trigger the note if it was already ON
+    if ((new_keys_state[i] & keys_state[i]) == 1)
+    {
+        if ((lastTouchMs - prevTouch) > HOLD_THRESHOLD_MS)
+        {
+          MIDI.sendNoteOff(C4 + i, VELOCITY, MIDI_CHANNEL);
+          MIDI.sendNoteOn(C4 + i, VELOCITY, MIDI_CHANNEL);
+        }
+    }
+
     if (new_keys_state[i] != keys_state[i])
     {
-      // Play / Stop the MIDI note
       if (new_keys_state[i] == 1)
-        MIDI.sendNoteOn(60 + i, 64, MIDI_CHANNEL);
+      {
+        MIDI.sendNoteOn(C4 + i, VELOCITY, MIDI_CHANNEL);
+        keys_state[i] = 1;
+      }
       else
-        MIDI.sendNoteOff(60 + i, 64, MIDI_CHANNEL);
-      
-      // Save the new state
-      keys_state[i] = new_keys_state[i];
+      {
+        if ((lastTouchMs - prevTouch) > HOLD_THRESHOLD_MS)
+        {
+          MIDI.sendNoteOff(C4 + i, VELOCITY, MIDI_CHANNEL);
+          keys_state[i] = 0;
+        }
+      }
     }
+
+    Serial.print(keys_state[i]);
   }
 
   Serial.println();
